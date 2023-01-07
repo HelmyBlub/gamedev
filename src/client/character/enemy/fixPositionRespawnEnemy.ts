b75748edb41aa2df439b34b19721e0cebff49ed2
Helmy Blub <helmyblub@tutanota.com>
2023-01-07 14:03:43 +0100

    different enemy based on map direction

--- src/client/character/enemy/fixPositionRespawnEnemy.ts
+++ src/client/character/enemy/fixPositionRespawnEnemy.ts
@@ -16,6 +16,21 @@ export type FixPositionRespawnEnemyCharacter = Character & {
     respawnTime: number,
 }
 
+type EnemyTypes = {
+    [key: string]: EnemyType
+}
+
+type EnemyType = {
+    hpFactor: number,
+    sizeFactor: number,
+}
+
+const ENEMY_TYPES: EnemyTypes = {
+    "big": {hpFactor: 2, sizeFactor: 1.5},
+    "default": {hpFactor: 1, sizeFactor: 1},
+    "small": {hpFactor: 0.5, sizeFactor: 0.75},
+}
+
 export function createFixPositionRespawnEnemiesOnInit(game: Game){
     let map = game.state.map;
     let existingMapKeys = Object.keys(map.chunks);
@@ -35,8 +50,8 @@ export function createFixPositionRespawnEnemies(chunk: MapChunk, chunkI: number,
     }
 
     let chunkSize = map.tileSize * map.chunkLength;
-    let mapCenter = {x:0, y:0};
-    let minSpawnDistanceFromMapCenter = 400;
+    let mapCenter = {x:chunkSize/2, y:chunkSize/2};
+    let minSpawnDistanceFromMapCenter = 500;
 
     let topLeftMapKeyPos: Position = {
         x: chunkJ * chunkSize,
@@ -46,19 +61,27 @@ export function createFixPositionRespawnEnemies(chunk: MapChunk, chunkI: number,
         x: topLeftMapKeyPos.x + chunkSize / 2,
         y: topLeftMapKeyPos.y + chunkSize / 2
     }
-    let distance = calculateDistance(mapCenter, centerMapKeyPos);
-    if (minSpawnDistanceFromMapCenter < distance) {
+    let chunkDistance = calculateDistance(mapCenter, centerMapKeyPos);
+    let enemyType: string;
+    if(chunkI > 0 && Math.abs(chunkI) >= Math.abs(chunkJ)){
+        enemyType = "big";
+    }else if(chunkI < 0 && Math.abs(chunkI) >= Math.abs(chunkJ)){
+        enemyType = "small";
+    }else{
+        enemyType = "default";
+    }
+    if (minSpawnDistanceFromMapCenter < chunkDistance + chunkSize) {
         for (let i = 0; i < chunk.tiles.length; i++) {
             for (let j = 0; j < chunk.tiles[i].length; j++) {
                 let enemyPos: Position = {
                     x: topLeftMapKeyPos.x + j * map.tileSize + map.tileSize / 2,
                     y: topLeftMapKeyPos.y + i * map.tileSize + map.tileSize / 2
                 }
-                distance = calculateDistance(mapCenter, enemyPos);
+                let distance = calculateDistance(mapCenter, enemyPos);
                 if (minSpawnDistanceFromMapCenter < distance) {
                     if (!isPositionBlocking(enemyPos, map, idCounter)) {
                         let level = Math.max(Math.floor((distance - minSpawnDistanceFromMapCenter) / 1000), 0) + 1;
-                        chunk.characters.push(createEnemyWithLevel(idCounter, enemyPos, level));
+                        chunk.characters.push(createEnemyWithLevel(idCounter, enemyPos, level, ENEMY_TYPES[enemyType]));
                     }
                 }
             }
@@ -157,11 +180,14 @@ function resetEnemy(enemy: FixPositionRespawnEnemyCharacter, map: GameMap) {
     delete enemy.respawnOnTime;
 }
 
-function createEnemyWithLevel(idCounter: IdCounter, enemyPos: Position, level: number){
+function createEnemyWithLevel(idCounter: IdCounter, enemyPos: Position, level: number, enemyType: EnemyType){
+    if(enemyType === undefined){
+        throw Error("enemy type unknwon" + enemyType);
+    }
     let colors = ["black", "green", "blue", "red"];
-    let hp = 5 * Math.pow(level, 3);
+    let hp = 5 * Math.pow(level, 3) * enemyType.hpFactor;
     let moveSpeed = Math.min(20, 1 + level/5);
-    let size = Math.min(40, 10 + 5 * Math.floor(level/colors.length + 1));
+    let size = Math.min(40, (10 + 5 * Math.floor(level/colors.length + 1)) * enemyType.sizeFactor);
     let damage = level;
     let color = colors[level%colors.length];
     let autoAggroRange = Math.min(750, 50 + level * 50); 
