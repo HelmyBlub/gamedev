f34bcf3fcbd19d0d315013393e92631d75804a2e
Helmy Blub <helmyblub@gmail.com>
2024-11-17 13:16:10 +0100

    activate test run

--- src/client/test/gameTest.ts
+++ src/client/test/gameTest.ts
@@ -113,7 +113,7 @@ function testPlayerClasses(game: Game) {
     replay.testInputFileQueue.push("/data/testReplayLongBuilder.json");
     replay.testInputFileQueue.push("/data/testReplayLongTamer.json");
     replay.testInputFileQueue.push("/data/testReplayLongBall.json");
-    // replay.testInputFileQueue.push("/data/testReplayLongMusician.json");
+    replay.testInputFileQueue.push("/data/testReplayLongMusician.json");
     // replay.testInputFileQueue.push("/data/testReplayMultiplayerLong.json");
     //replay.testInputFileQueue.push("/data/testReplayLongAll.json");
 
