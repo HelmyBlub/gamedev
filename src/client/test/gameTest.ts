e33d8ff53fd0557d51f1f4ee506346a0df9b45dd
Helmy Blub <helmyblub@gmail.com>
2024-12-01 13:13:29 +0100

    fix short replays

--- src/client/test/gameTest.ts
+++ src/client/test/gameTest.ts
@@ -99,7 +99,7 @@ function testPlayerClasses(game: Game) {
     const replay = game.testing.replay;
     replay.testInputFileQueue = [];
     //    replay.testInputFileQueue.push("/data/testInputError.json");    
-    replay.testInputFileQueue.push("/data/testReplayShortRetry.json");
+    //replay.testInputFileQueue.push("/data/testReplayShortRetry.json");
     replay.testInputFileQueue.push("/data/testReplayShortSniper.json");
     replay.testInputFileQueue.push("/data/testReplayShortBuilder.json");
     replay.testInputFileQueue.push("/data/testReplayShortTamer.json");
@@ -109,11 +109,11 @@ function testPlayerClasses(game: Game) {
 
     // replay.testInputFileQueue.push("/data/testReplayMultiplayer.json");
 
-    replay.testInputFileQueue.push("/data/testReplayLongSniper.json");
-    replay.testInputFileQueue.push("/data/testReplayLongBuilder.json");
-    replay.testInputFileQueue.push("/data/testReplayLongTamer.json");
-    replay.testInputFileQueue.push("/data/testReplayLongBall.json");
-    replay.testInputFileQueue.push("/data/testReplayLongMusician.json");
+    // replay.testInputFileQueue.push("/data/testReplayLongSniper.json");
+    // replay.testInputFileQueue.push("/data/testReplayLongBuilder.json");
+    // replay.testInputFileQueue.push("/data/testReplayLongTamer.json");
+    // replay.testInputFileQueue.push("/data/testReplayLongBall.json");
+    // replay.testInputFileQueue.push("/data/testReplayLongMusician.json");
     // replay.testInputFileQueue.push("/data/testReplayMultiplayerLong.json");
     //replay.testInputFileQueue.push("/data/testReplayLongAll.json");
 
