038f4cfea80e39b044d27cd6fbf2aeae3b5a5fcc
Helmy Blub <helmyblub@gmail.com>
2024-12-08 12:54:17 +0100

    new musician test run

--- src/client/test/gameTest.ts
+++ src/client/test/gameTest.ts
@@ -77,8 +77,8 @@ export function testGame(game: Game) {
 export function initReplay(): Replay {
     return {
         startTime: performance.now(),
-        frameSkipAmount: 0,
-        zeroTimeout: false,
+        frameSkipAmount: 60,
+        zeroTimeout: true,
     }
 }
 
@@ -98,7 +98,7 @@ function testPlayerClasses(game: Game) {
 
     const replay = game.testing.replay;
     replay.testInputFileQueue = [];
-    replay.testInputFileQueue.push("/data/testInputError.json");
+    //    replay.testInputFileQueue.push("/data/testInputError.json");    
     //replay.testInputFileQueue.push("/data/testReplayShortRetry.json");
     replay.testInputFileQueue.push("/data/testReplayShortSniper.json");
     replay.testInputFileQueue.push("/data/testReplayShortBuilder.json");
@@ -113,7 +113,7 @@ function testPlayerClasses(game: Game) {
     replay.testInputFileQueue.push("/data/testReplayLongBuilder.json");
     // replay.testInputFileQueue.push("/data/testReplayLongTamer.json");
     replay.testInputFileQueue.push("/data/testReplayLongBall.json");
-    // replay.testInputFileQueue.push("/data/testReplayLongMusician.json");
+    replay.testInputFileQueue.push("/data/testReplayLongMusician.json");
     // replay.testInputFileQueue.push("/data/testReplayMultiplayerLong.json");
     //replay.testInputFileQueue.push("/data/testReplayLongAll.json");
 
