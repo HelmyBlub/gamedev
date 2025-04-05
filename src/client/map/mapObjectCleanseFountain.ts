f4d7ec1a313af7f94057711002af35a7e8eff12d
Helmy Blub <helmyblub@gmail.com>
2025-04-05 12:16:59 +0200

    fountain image

--- src/client/map/mapObjectCleanseFountain.ts
+++ src/client/map/mapObjectCleanseFountain.ts
@@ -1,6 +1,6 @@
 import { Character } from "../character/characterModel.js";
 import { getCameraPosition } from "../game.js";
-import { Game } from "../gameModel.js";
+import { Game, Position } from "../gameModel.js";
 import { getPointPaintPosition, paintTextLinesWithKeys } from "../gamePaint.js";
 import { mapKeyAndTileXYToPosition } from "./map.js";
 import { MAP_OBJECTS_FUNCTIONS, MapTileObject, findMapKeyForMapObject } from "./mapObjects.js";
@@ -8,17 +8,25 @@ import { findPlayerByCharacterId } from "../player.js";
 import { IMAGE_SIGN } from "./mapObjectSign.js";
 import { playerInputBindingToDisplayValue } from "../input/playerInput.js";
 import { areaSpawnOnDistanceFightStart } from "./mapAreaSpawnOnDistance.js";
+import { GAME_IMAGES, loadImage } from "../imageLoad.js";
 
 export type MapTileObjectCleanseFountain = MapTileObject & {
     used: boolean,
     areaIfRef: number,
 }
 export const MAP_OBJECT_CLEANSE_FOUNTAIN = "CleanseFountain";
+export const IMAGE_FOUNTAIN = "FOuntain";
+GAME_IMAGES[IMAGE_FOUNTAIN] = {
+    imagePath: "/images/fountain.png",
+    spriteRowHeights: [100],
+    spriteRowWidths: [100],
+};
 
 export function addMapObjectCleanseFountain() {
     MAP_OBJECTS_FUNCTIONS[MAP_OBJECT_CLEANSE_FOUNTAIN] = {
         interact1: interactCleanse,
         paintInteract: paintInteract,
+        paint: paint,
     }
 }
 
@@ -31,6 +39,30 @@ export function mapObjectCreateCleanseFountain(tileX: number, tileY: number, are
         image: IMAGE_SIGN,
         used: false,
         areaIfRef: areaIfRef,
+    }
+}
+
+function paint(ctx: CanvasRenderingContext2D, mapObject: MapTileObject, paintTopLeft: Position, game: Game) {
+    const tileSize = game.state.map.tileSize;
+    const fountainImage = GAME_IMAGES[IMAGE_FOUNTAIN];
+    loadImage(fountainImage);
+    if (fountainImage.imageRef?.complete) {
+        const paintX = paintTopLeft.x + mapObject.y * tileSize;
+        const paintY = paintTopLeft.y + mapObject.x * tileSize;
+        const spriteAnimation = Math.floor(game.state.time / 100) % 3;
+        const spriteWidth = fountainImage.spriteRowWidths[0];
+        const spriteHeight = fountainImage.spriteRowHeights[0];
+        const xOffset = (spriteWidth - tileSize) / 2;
+        const yOffset = (spriteHeight - tileSize) / 2;
+        ctx.drawImage(
+            fountainImage.imageRef,
+            0 + spriteAnimation * spriteWidth,
+            0,
+            spriteWidth, spriteHeight,
+            Math.floor(paintX - xOffset),
+            Math.floor(paintY - yOffset),
+            spriteWidth, spriteHeight
+        );
     }
 }
 
