6711179dbc4cf0c282c01b9bcb6b3f6fd0aa9b97
Helmy Blub <helmyblub@gmail.com>
2025-05-04 11:31:32 +0200

    fixed tower ability on fountain boss. New test run long ball

--- src/client/ability/abilityTower.ts
+++ src/client/ability/abilityTower.ts
@@ -20,6 +20,8 @@ import { ABILITY_NAME_SINGLETARGET } from "./abilitySingleTarget.js";
 import { ABILITY_NAME_SWORD } from "./abilitySword.js";
 import { MoreInfoPart, createMoreInfosPart } from "../moreInfo.js";
 import { AbilityDamageBreakdown } from "../combatlog.js";
+import { CHARACTER_TYPE_AREA_BOSS } from "../character/enemy/areaBoss/areaBoss.js";
+import { CHARACTER_TYPE_CURSE_FOUNTAIN_BOSS } from "../character/enemy/curseFountainBoss.js";
 
 type AbilityObjectTower = AbilityObject & {
     ownerId: number,
@@ -207,7 +209,7 @@ function castTower(abilityOwner: AbilityOwner, ability: Ability, castPosition: P
     const newTower: AbilityObjectTower = createAbilityObjectTower(game.state.idCounter, abilityOwner.id, abilityOwner.faction, castPosition, nextAbility, abilityTower.damage);
     newTower.abilityIdRef = ability.id;
     if (abilityOwner.faction === FACTION_ENEMY) {
-        if (abilityOwner.type === CHARACTER_TYPE_BOSS_ENEMY || abilityOwner.type === CHARACTER_TYPE_KING_ENEMY) {
+        if (abilityOwner.type === CHARACTER_TYPE_BOSS_ENEMY || abilityOwner.type === CHARACTER_TYPE_KING_ENEMY || abilityOwner.type === CHARACTER_TYPE_CURSE_FOUNTAIN_BOSS) {
             newTower.isBossTower = true;
         } else if (abilityOwner.type === CHARACTER_TYPE_ENEMY_FIX_RESPAWN_POSITION) {
             const enemy: FixPositionRespawnEnemyCharacter = abilityOwner as any;
--- src/client/main.ts
+++ src/client/main.ts
@@ -23,7 +23,7 @@ var gameCount: number = 0;
 export const GAME_VERSION: GameVersion = {
     major: 0,
     minor: 4,
-    patch: 359,
+    patch: 360,
 }
 
 export function start() {
