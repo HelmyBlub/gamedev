23b8adeb3b7d25c8d95dcd2d8fdb9bd299595e5b
Helmy Blub <helmyblub@gmail.com>
2025-08-24 10:56:10 +0200

    new test run tamer

--- src/client/test/gameTest.ts
+++ src/client/test/gameTest.ts
@@ -108,7 +108,7 @@ function testPlayerClasses(game: Game) {
 
     replay.testInputFileQueue.push("/data/testReplayLongSniper.json");
     replay.testInputFileQueue.push("/data/testReplayLongBuilder.json");
-    // replay.testInputFileQueue.push("/data/testReplayLongTamer.json");
+    replay.testInputFileQueue.push("/data/testReplayLongTamer.json");
     replay.testInputFileQueue.push("/data/testReplayLongBall.json");
     // replay.testInputFileQueue.push("/data/testReplayLongMusician.json");
     // replay.testInputFileQueue.push("/data/testReplayHardModeGodKill.json");
