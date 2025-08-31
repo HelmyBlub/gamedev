3644f6208ae1b39e5e0fd96b9b0a9619b7dd96e6
Helmy Blub <helmyblub@gmail.com>
2025-08-31 11:29:05 +0200

    base defense teleport bots back to middle when to far away. New test run tamer.

--- src/client/character/playerCharacters/characterBot.ts
+++ src/client/character/playerCharacters/characterBot.ts
@@ -1,5 +1,9 @@
-import { ABILITIES_FUNCTIONS } from "../../ability/ability.js";
+import { ABILITIES_FUNCTIONS, resetAllCharacterAbilities } from "../../ability/ability.js";
+import { calculateDistance } from "../../game.js";
+import { GAME_MODE_BASE_DEFENSE } from "../../gameModeBaseDefense.js";
 import { Game } from "../../gameModel.js";
+import { getMapMidlePosition } from "../../map/map.js";
+import { resetCharacter, teleportCharacterPetsToOwner } from "../character.js";
 import { Character, CHARACTER_TYPE_FUNCTIONS } from "../characterModel.js";
 import { PathingCache } from "../pathing.js";
 
@@ -13,6 +17,16 @@ export function addCharacterTypeBotFunctions() {
 
 function tick(character: Character, game: Game, pathingCache: PathingCache | null) {
     if (character.state !== "alive") return;
+    if (game.state.gameMode === GAME_MODE_BASE_DEFENSE) {
+        const mapMiddle = getMapMidlePosition(game.state.map);
+        const distance = calculateDistance(mapMiddle, character);
+        if (distance > 1500) {
+            character.x = mapMiddle.x;
+            character.y = mapMiddle.y;
+            resetCharacter(character, game);
+            teleportCharacterPetsToOwner([character], game);
+        }
+    }
     for (let ability of character.abilities) {
         let functions = ABILITIES_FUNCTIONS[ability.name];
         if (functions.tickAI) functions.tickAI(character, ability, game);
--- src/client/main.ts
+++ src/client/main.ts
@@ -23,7 +23,7 @@ var gameCount: number = 0;
 export const GAME_VERSION: GameVersion = {
     major: 0,
     minor: 4,
-    patch: 393,
+    patch: 394,
 }
 
 export function start() {
