79ae5bde26fbe2a56815a5dde044888c019f5b89
Helmy Blub <helmyblub@gmail.com>
2026-04-05 12:08:08 +0200

    comment out lines for itchio

--- src/client/main.ts
+++ src/client/main.ts
@@ -79,8 +79,8 @@ export function createGame(canvasElementId: string | undefined, forTesting: bool
     } else {
         game = createDefaultGameData(undefined, undefined);
     }
-    toggleTestMenu(false);
-    game.multiplayer.disabled = true;
+    // toggleTestMenu(false);
+    // game.multiplayer.disabled = true;
     runner(game);
     return game;
 }
